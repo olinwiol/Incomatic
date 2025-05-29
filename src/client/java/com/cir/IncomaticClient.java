516364956ec21d4e743ad605454ba2e19831870f
olinwiol <dotsomewhat@gmail.com>
2025-05-29 12:45:49 +0200

    Initial commit

---
+++ src/client/java/com/cir/IncomaticClient.java
@@ -0,0 +1,10 @@
+package com.cir;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IncomaticClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
